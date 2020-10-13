fff3964de74e568064e32f9058a8ee8f60d52ad3
Owen Voke <development@voke.dev>
2020-10-13 16:22:10 +0100

    feat: add 'diff' syntax highlighting

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -4,6 +4,7 @@ import hljs from 'highlight.js/lib/highlight';
 
 hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'));
 hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'));
 hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'));
 hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'));
 hljs.registerLanguage('json', require('highlight.js/lib/languages/json'));
